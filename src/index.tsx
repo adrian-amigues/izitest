0053b1dca77b3aaf317a8db7ee4de97922e66c01
adrian-amigues <adrian.amigues@gmail.com>
2021-06-06 15:44:47 +0200

    Initialize project using Create React App

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+`
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
